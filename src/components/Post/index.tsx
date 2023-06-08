e57154b240a1fdd1afcd61a119ab997420d78a1c
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-08 17:44:36 -0300

    Rota aninhada

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,14 +1,10 @@
-import { useParams, useSearchParams } from 'react-router-dom';
 import './style.css';
 
 export const Post = () => {
-    const params = useParams();
-    const { id } = params;
-    const [qs] = useSearchParams();
-
     return (
     <div>
-        <h1>Post {`Para: ${id}`} {`QS: ${qs.get('user')}`}</h1>
+        <h1>Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptatibus sunt ab amet eveniet repudiandae sapiente dicta assumenda. Debitis ipsa repudiandae iusto excepturi dolorum. Velit illo magnam assumenda eveniet similique.</p>
     </div>
     )
 }
