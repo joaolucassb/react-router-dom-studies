19ebe518ae1187c713be7b5f8bf109cee917a2fc
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-08 17:09:10 -0300

    UseParams Routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Menu } from './components/Menu';
+import { Post } from './components/Post';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -14,6 +15,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/posts/:id" element={<Post />} />
+        <Route path="/posts" element={<Post />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
