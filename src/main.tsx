c1645117627b23d78a35b774480927ca56ddfbe9
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-06-08 16:41:01 -0300

    Add Changes

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './sytles/global.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Menu } from './components/Menu';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
